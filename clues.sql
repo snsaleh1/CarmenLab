f45657e780a0732428eb6ff17dcf7004debd866f
snsaleh1 <snsaleh11@gmail.com>
2019-12-16 22:08:09 -0600

    5 solved

--- clues.sql
+++ clues.sql
@@ -40,6 +40,7 @@
 
 -- Write SQL query here
 
+-- SELECT o.name FROM city c LEFT JOIN country o ON c.countrycode = o.code where c.name LIKE 'Serr%';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
